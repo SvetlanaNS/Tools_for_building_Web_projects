723a3dc3f62081d809be79276e836847a0717c93
Svetlana <knsveta@mail.ru>
2022-12-23 15:08:23 +0300

    lesson_2

---
+++ lesson 2/src/main.js
@@ -0,0 +1,26 @@
+import { diffDates, diffToHtml } from "./datecalc.js";
+import { formatError } from "./utils.js";
+
+import chunk from "lodash/chunk";
+
+console.log(chunk(["a", "b", "c", "d"]), 2)
+
+
+const dateCalcForm = document.getElementById("datecalc"); 
+const dateCalcResult = document.getElementById("datecalc__result"); 
+
+dateCalcForm.addEventListener("submit", handleCalcDates); 
+
+function handleCalcDates(event) { 
+    dateCalcResult.innerHTML = ""; 
+    event.preventDefault(); 
+
+    let { firstDate, secondDate } = event.target.elements; 
+    firstDate = firstDate.value, secondDate = secondDate.value; 
+    
+    if (firstDate && secondDate) { 
+        const diff = diffDates(firstDate, secondDate);
+        dateCalcResult.innerHTML = diffToHtml(diff);
+    }
+    else dateCalcResult.innerHTML = formatError("Для расчета промежутка необходимо заполнить оба поля");
+}
---
+++ lesson 2/webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { resolve } = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        filename: 'main.bundle.js',
+    },
+    plugins: [
+        new BundleAnalyzerPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
+        new HtmlWebpackPlugin({
+            template: resolve(__dirname, 'index.html')
+        })
+    ],
+    module: {
+        rules: [
+            { test: /\\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'] 
+            },
+            { test: /\\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] 
+            },
+        ]
+    },
+devServer: {
+       port: 5500
+    }
+  
+
+};
